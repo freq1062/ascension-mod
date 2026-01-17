7db0fe94a9c589c6acfbf02a09b0fb62ffc773c2
freq1062 <alexminth@gmail.com>
2026-01-17 10:41:13 -0500

    initial commit

---
+++ src/client/java/freq/ascension/AscensionClient.java
@@ -0,0 +1,10 @@
+package freq.ascension;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AscensionClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/freq/ascension/AscensionDataGenerator.java
@@ -0,0 +1,11 @@
+package freq.ascension;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AscensionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
