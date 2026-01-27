cd883367c830c830b530ef4008551bb729293d92
Alex Min <alex.min@mail.utoronto.ca>
2026-01-26 23:55:41 -0500

    Book ascension menu concept

--- src/main/java/freq/ascension/menus/AscensionMenu.java
+++ src/main/java/freq/ascension/menus/AscensionMenu.java
@@ -2,100 +2,77 @@ package freq.ascension.menus;
 
 import java.util.List;
 
-import eu.pb4.sgui.api.elements.GuiElementBuilder;
-import eu.pb4.sgui.api.gui.SimpleGui;
+import eu.pb4.sgui.api.gui.BookGui;
 import freq.ascension.Utils;
-import freq.ascension.items.InfluenceItem;
 import freq.ascension.managers.AscensionData;
 import freq.ascension.orders.Order;
-import freq.ascension.registry.OrderRegistry;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.inventory.MenuType;
+import net.minecraft.server.network.Filterable;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.component.ItemLore;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.component.WrittenBookContent;
 
 public class AscensionMenu {
         public void open(ServerPlayer player) {
-                SimpleGui gui = new SimpleGui(MenuType.GENERIC_9x1, player, false);
-                gui.setTitle(Component.literal(Utils.smallCaps("Ascension SMP"))
-                                .withStyle(Style.EMPTY.withColor(ChatFormatting.DARK_PURPLE).withItalic(false)));
+                AscensionData data = (AscensionData) player;
+                Order passive = data.getPassive();
+                Order utility = data.getUtility();
+                Order combat = data.getCombat();
 
-                update(player, gui);
+                Component pageContent = Component.empty()
+                                .append(Component.literal("   " + Utils.smallCaps("Ascension SMP"))
+                                                .withStyle(Style.EMPTY.withColor(ChatFormatting.DARK_PURPLE)
+                                                                .withBold(true)))
+                                .append("\n")
+                                .append(Component.literal("Influence: " + data.getInfluence())
+                                                .withStyle(ChatFormatting.BLACK))
+                                .append("\n")
+                                .append(Component.literal("Passive: ").withStyle(ChatFormatting.BLACK))
+                                .append(passive != null
+                                                ? Component.literal(Utils.smallCaps(passive.getOrderName()))
+                                                                .withColor(passive.getOrderColor().getValue())
+                                                : Component.literal("None").withStyle(ChatFormatting.GRAY))
+                                .append("\n")
+                                .append(Component.literal("Utility: ").withStyle(ChatFormatting.BLACK))
+                                .append(utility != null
+                                                ? Component.literal(Utils.smallCaps(utility.getOrderName()))
+                                                                .withColor(utility.getOrderColor().getValue())
+                                                : Component.literal("None").withStyle(ChatFormatting.GRAY))
+                                .append("\n")
+                                .append(Component.literal("Combat: ").withStyle(ChatFormatting.BLACK))
+                                .append(combat != null
+                                                ? Component.literal(Utils.smallCaps(combat.getOrderName()))
+                                                                .withColor(combat.getOrderColor().getValue())
+                                                : Component.literal("None").withStyle(ChatFormatting.GRAY))
+                                // \n\n\n\n\n\uF802\uF803
+                                .append("\n")
+                                .append(Component
+                                                .literal("\uF806\uE181\uF801\uF801\uF801\uF801\uF801\uF802\uF802\uF804G\n\uF806"
+                                                                + //
+                                                                "   \uF804A\uF802\uF802    B\n\uF806" + //
+                                                                "\n" + //
+                                                                "\n\uF806" + //
+                                                                " \uF804\uF804\uF804C\uF802\uF802\uF802  \uF804 D\n"
+                                                                + //
+                                                                "\n" + //
+                                                                "\n\uF806" + //
+                                                                "   E\uF802\uF802   \uF804 F\n\uF806" + //
+                                                                "\uF802\uF802  H") // forwards
+                                                .withStyle(Style.EMPTY.withColor(0xFFFFFF)));
+
+                ItemStack book = new ItemStack(Items.WRITTEN_BOOK);
+
+                book.set(DataComponents.WRITTEN_BOOK_CONTENT, new WrittenBookContent(
+                                Filterable.passThrough("AscensionMenu"),
+                                "freq1062", 3, List.of(
+                                                Filterable.passThrough(pageContent)),
+                                true));
+
+                BookGui gui = new BookGui(player, book);
                 gui.open();
-        }
-
-        public static void update(ServerPlayer player, SimpleGui gui) {
-                AscensionData data = (AscensionData) player;
-                // Setting items in the container associated with the menu
-                gui.setSlot(0, InfluenceItem.getInfluenceDisplayItem(data));
-
-                int i = 0;
-                Iterable<Order> orders = OrderRegistry.iterable();
-                if (orders != null) {
-                        for (Order order : orders) {
-                                if (i + 2 < 9) { // Ensure we don't go out of bounds
-                                        gui.setSlot(i + 2, new GuiElementBuilder(makeIcon(order, player))
-                                                        .setCallback((index, type, action) -> {
-                                                                new OrderDetailMenu(order.getOrderName()).open(player);
-                                                                // play sound?
-                                                        }));
-                                }
-                                i++;
-                        }
-                }
-        }
-
-        private static ItemStack makeIcon(Order order, Player player) {
-                AscensionData data = (AscensionData) player;
-                boolean passiveUnlocked = data.getUnlockedOrder(order.getOrderName()).hasPassive();
-                boolean utilityUnlocked = data.getUnlockedOrder(order.getOrderName()).hasUtility();
-                boolean combatUnlocked = data.getUnlockedOrder(order.getOrderName()).hasCombat();
-
-                ItemStack item = order.getOrderItem().copy(); // Make sure to copy if getOrderItem returns a static
-                                                              // reference
-
-                item.set(DataComponents.ITEM_NAME, Component.literal(
-                                Utils.smallCaps(order.getOrderName()))
-                                .withColor(order.getOrderColor().getValue())
-                                .withStyle(ChatFormatting.BOLD));
-
-                item.set(DataComponents.LORE, new ItemLore(
-                                List.of(
-                                                Component.literal("Click to view abilities")
-                                                                .withStyle(style -> style
-                                                                                .withItalic(false)
-                                                                                .withColor(ChatFormatting.GRAY)),
-
-                                                Component.literal("Passive")
-                                                                .withStyle(style -> style
-                                                                                .withColor(passiveUnlocked
-                                                                                                ? ChatFormatting.GREEN
-                                                                                                : ChatFormatting.RED)
-                                                                                .withItalic(false)
-                                                                                .withBold(data.getPassive() == order)),
-
-                                                Component.literal("Utility")
-                                                                .withStyle(style -> style
-                                                                                .withColor(utilityUnlocked
-                                                                                                ? ChatFormatting.GREEN
-                                                                                                : ChatFormatting.RED)
-                                                                                .withItalic(false)
-                                                                                .withBold(data.getUtility() == order)),
-
-                                                Component.literal("Combat")
-                                                                .withStyle(style -> style
-                                                                                .withColor(combatUnlocked
-                                                                                                ? ChatFormatting.GREEN
-                                                                                                : ChatFormatting.RED)
-                                                                                .withItalic(false)
-                                                                                .withBold(data.getCombat() == order))
-
-                                )));
-                return item;
         }
 }
